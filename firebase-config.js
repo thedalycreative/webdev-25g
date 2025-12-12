2e02259febf4ac421a554e20ef50768848c5a329
Tim | EQC Trainer <tim.eqctrainer@Tims-MacBook-Pro.local>
2025-12-12 13:15:00 +0800

    Add Firebase configuration for realtime features

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase configuration for webdev-25g-quokkas
+// This file is loaded optionally by typing-test.html and noticeboard.html
+
+const firebaseConfig = {
+  apiKey: "AIzaSyATlCI5ihHL2SNHM9iQh_Sbl5sUvIkeZ7o",
+  authDomain: "webdev-25g-quokkas.firebaseapp.com",
+  projectId: "webdev-25g-quokkas",
+  storageBucket: "webdev-25g-quokkas.firebasestorage.app",
+  messagingSenderId: "896945386948",
+  appId: "1:896945386948:web:5e73d8d76a1b1800e6a091"
+};
